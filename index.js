3e7e8e3d57c1b3d4dc22dd8765a48d22d4e8bf89
paul61843@gmail.com <paul61843@gmail.com>
2022-08-31 06:24:24 +0000

    add post

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require("express");
+const bodyParser = require('body-parser');
 const app = express();
+
+app.use(bodyParser.json()); 
 
 app.get("/", (req, res) => {
     res.send("Express on Vercel");
 });
 
 app.post("/hello", (req, res) => {
-    res.send("hello", req.data);
+    const data = req.body
+    console.log(data);
+    res.send(200, 'hello' + JSON.stringify(data));
 });
 
 app.listen(3456, () => {
