79adf000f7f140466336ddd431862123941de6aa
paul61843@gmail.com <paul61843@gmail.com>
2022-08-31 05:58:32 +0000

    add post

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const app = express();
+
 app.get("/", (req, res) => {
-res.send("Express on Vercel");
+    res.send("Express on Vercel");
 });
+
+app.post("/hello", (req, res) => {
+    res.send("hello", req.data);
+});
+
 app.listen(3456, () => {
-console.log("Running on port 3456.");
+    console.log("Running on port 3456.");
 });
+
 // Export the Express API
 module.exports = app;
